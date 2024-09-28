4df03de7f82f04886e4495325f18e81fb5c8c35b
landwarrior <tech_land_equilibrium@outlook.jp>
2024-09-28 23:15:06 +0900

    google.comでも同じっぽいので統一

--- output_map_search_result/popup.js
+++ output_map_search_result/popup.js
@@ -11,27 +11,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function extractAndDownload() {
     const results = [];
-    if (location.href.includes('https://www.google.com/maps')) {
-        const elements = document.querySelectorAll('.section-result');
-
-        elements.forEach((element) => {
-            const name = element.querySelector('.section-result-title span').innerText;
-            const address = element.querySelector('.section-result-location').innerText;
-            const phone = element.querySelector('.section-result-phone-number').innerText;
-            results.push({ name, address, phone });
-        });
-    } else {
-        const elementBase = document.querySelector('div[aria-label*="検索結果"][role="feed"]');
-
-        for (const element of elementBase.querySelectorAll('div:not([class]):not([jslog])')) {
-            const anchor = element.querySelector('a');
-            if (!anchor) {
-                continue;
-            }
-            const name = anchor.getAttribute('aria-label');
-            const address = anchor.href;
-            results.push({ name, address });
+    const elementBase = document.querySelector('div[aria-label*="検索結果"][role="feed"]');
+    for (const element of elementBase.querySelectorAll('div:not([class]):not([jslog])')) {
+        const anchor = element.querySelector('a');
+        if (!anchor) {
+            continue;
         }
+        const name = anchor.getAttribute('aria-label');
+        const address = anchor.href;
+        results.push({ name, address });
     }
     const csvContent = 'data:text/csv;charset=utf-8,' + results.map((e) => `${e.name},${e.address}`).join('\n');
 
